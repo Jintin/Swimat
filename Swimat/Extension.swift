d2f80c8596e9e18c0bc26515c143661d0cce4110
jintin <jintinapps@gmail.com>
2016-03-21 22:16:10 +0800

    * update test

--- Swimat/Extension.swift
+++ Swimat/Extension.swift
@@ -4,18 +4,18 @@ extension String {
 	func findDiff(string: String) -> (start: Int, end: Int) {
 		var start = 0
 		var end = 0
-		let minValue = min(self.characters.count, string.characters.count)
+		let minValue = min(characters.count, string.characters.count)
 		if minValue == 0 {
 			return (0, 0)
 		}
-		while self[self.startIndex.advancedBy(start)] == string[string.startIndex.advancedBy(start)] {
+		while self[startIndex.advancedBy(start)] == string[string.startIndex.advancedBy(start)] {
 			if start < minValue - 1 {
 				start++
 			} else {
 				break
 			}
 		}
-		while self[self.endIndex.advancedBy(-end - 1)] == string[string.endIndex.advancedBy(-end - 1)] {
+		while self[endIndex.advancedBy(-end - 1)] == string[string.endIndex.advancedBy(-end - 1)] {
 			if minValue - end - 1 >= start {
 				end++
 			} else {
