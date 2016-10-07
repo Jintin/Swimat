ac3a6643098e7f47e3762df3307b6b1b0121e3a1
Saagar Jha <saagar@saagarjha.com>
2016-10-07 13:02:22 -0700

    Adds some command line options
    Namely, -i to specify indent size and -f to force formatting

--- Swimat/main.swift
+++ Swimat/main.swift
@@ -9,13 +9,47 @@
 import CoreServices
 import Foundation
 
-SwiftParser.indentChar = "\t"
+var indentSize = 0
+var indent: String {
+	if indentSize <= 0 {
+		return "\t"
+	} else {
+		return String(repeating: " ", count: indentSize)
+	}
+}
+var force = false
 
-for path in CommandLine.arguments.dropFirst() {
-	let file = URL(fileURLWithPath: path)
-	if UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (file.pathExtension) as CFString, nil)?.takeRetainedValue() as? String ?? "" == "public.swift-source" {
-		let parser = SwiftParser(string: try String(contentsOf: file))
-		let formattedText = try parser.format()
-		try formattedText.write(to: file, atomically: true, encoding: .utf8)
+var lookingForIndent = false
+
+for var argument in CommandLine.arguments.dropFirst() {
+	if argument.hasPrefix("-") {
+		argument.remove(at: argument.startIndex)
+		switch argument {
+		case "f":
+			force = !force
+		case "i":
+			lookingForIndent = true
+		default:
+			fatalError("-\(argument) is not a valid option.\nValid options are -i and -f.")
+		}
+	} else {
+		if (lookingForIndent) {
+			guard let size = Int(argument) else {
+				fatalError("\(argument) is not a valid indent size. Exiting.")
+			}
+			indentSize = size
+			lookingForIndent = false
+		} else {
+			let file = URL(fileURLWithPath: argument)
+			if force || UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (file.pathExtension) as CFString, nil)?.takeRetainedValue() as? String ?? "" == "public.swift-source" {
+				SwiftParser.indentChar = indent
+				let parser = SwiftParser(string: try String(contentsOf: file))
+				let formattedText = try parser.format()
+				try formattedText.write(to: file, atomically: true, encoding: .utf8)
+				print("\(argument) was formatted successfully.")
+			} else {
+				print("\(argument) doesn't appear to be a Swift file. Skipping.")
+			}
+		}
 	}
 }
