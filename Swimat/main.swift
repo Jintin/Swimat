a7d4709322136b2c9de07d0526f4e44a6e11fc89
Saagar Jha <saagar@saagarjha.com>
2016-12-04 12:24:48 -0800

    More flexible option parsing
    Also a bit of code refactoring into the new OptionParser and ErrorHandling files

--- Swimat/main.swift
+++ Swimat/main.swift
@@ -1,71 +1,20 @@
 import CoreServices
 import Foundation
 
-enum SwimatError: Int32 {
-    case noArguments = 1
-    case invalidOption
-    case invalidIndent
-}
-
-var indentSize = 0
-var indent: String {
-    if indentSize <= 0 {
-        return "\t"
+let options = Options.shared
+let paths = options.parseArguments(Array(CommandLine.arguments.dropFirst()))
+for path in paths {
+    let file = URL(fileURLWithPath: path)
+    if FileManager.default.fileExists(atPath: file.path),
+		let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension,
+                                                       (file.pathExtension) as CFString,
+                                                       nil)?.takeRetainedValue(),
+        uti == "public.swift-source" as CFString {
+            let parser = SwiftParser(string: try String(contentsOf: file))
+            let formattedText = try parser.format()
+            try formattedText.write(to: file, atomically: true, encoding: .utf8)
+            print("\(path) was formatted successfully.")
     } else {
-        return String(repeating: " ", count: indentSize)
-    }
-}
-
-var lookingForIndent = false
-
-func printToError(_ string: String = "") {
-    guard let data = "\(string)\n".data(using: .utf8) else {
-        return
-    }
-    FileHandle.standardError.write(data)
-}
-
-var files = [URL]()
-
-if CommandLine.arguments.count < 2 {
-    printToError("The Swimat Swift formatter")
-    printToError()
-    printToError("USAGE: swimat [options] <inputs...>")
-    printToError()
-    printToError("OPTIONS:")
-    printToError("-i <value>=0 Set the number of spaces to indent; use 0 for tabs.")
-    exit(SwimatError.noArguments.rawValue)
-}
-for var argument in CommandLine.arguments.dropFirst() {
-    if argument.hasPrefix("-") {
-        argument.remove(at: argument.startIndex)
-        switch argument {
-        case "i":
-            lookingForIndent = true
-        default:
-            printToError("-\(argument) is not a valid option.\nValid options are -i.")
-            exit(SwimatError.invalidOption.rawValue)
-        }
-    } else {
-        if (lookingForIndent) {
-            guard let size = Int(argument) else {
-                printToError("\(argument) is not a valid indent size. Exiting.")
-                exit(SwimatError.invalidIndent.rawValue)
-            }
-            indentSize = size
-            lookingForIndent = false
-        } else {
-            let file = URL(fileURLWithPath: argument)
-            if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (file.pathExtension) as CFString, nil)?.takeRetainedValue(),
-                uti == "public.swift-source" as CFString {
-                    SwiftParser.indentChar = indent
-                    let parser = SwiftParser(string: try String(contentsOf: file))
-                    let formattedText = try parser.format()
-                    try formattedText.write(to: file, atomically: true, encoding: .utf8)
-                    print("\(argument) was formatted successfully.")
-            } else {
-                    print("\(argument) doesn't appear to be a Swift file. Skipping.")
-            }
-        }
+            print("\(path) doesn't appear to be a Swift file. Skipping.")
     }
 }
