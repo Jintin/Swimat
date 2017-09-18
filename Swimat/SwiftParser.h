044d682ebefb9779b975c76f026eba3acee5d5d1
Jintin Lin <jintin.lin1018@gmail.com>
2017-09-18 21:17:47 +0800

    remove objc file

--- Swimat/SwiftParser.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "Parser.h"
-
-@interface SwiftParser : Parser
-
-
--(NSString*) formatString:(NSString*) string withRange:(NSRange) range;
-@end
--- Swimat/category/NSDocument+Common.h
+++
@@ -1,5 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface NSDocument (Common)
-
-@end
